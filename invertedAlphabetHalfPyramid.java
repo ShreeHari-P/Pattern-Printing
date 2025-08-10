111fc8233035709b84a0db4815d60ae364c2a6f7
Shree Hari P <shreehariparthiban159@gmail.com>
2025-08-10 20:25:52 +0530

    Inverted Half Pyramid - Alphabet2

---
+++ invertedAlphabetHalfPyramid.java
@@ -0,0 +1,13 @@
+public class invertedAlphabetHalfPyramid {
+    public static void main(String[] args){
+         char ch = 'A';
+
+        for(int i = 5; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print(ch+ " ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
