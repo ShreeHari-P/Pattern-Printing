5c956c754252044c8edc5fec76a3dc0e9ff19ede
Shree Hari P <shreehariparthiban159@gmail.com>
2025-08-13 07:56:03 +0530

    Floyd's Triangle

---
+++ floydTriangle.java
@@ -0,0 +1,13 @@
+public class floydTriangle {
+    public static void main(String[] args){
+        int number = 1, rows = 4;
+
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
