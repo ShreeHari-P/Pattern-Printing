c78f4319b8b8c5b1e1faa56f8412c0fb6ef70a71
Shree Hari P <shreehariparthiban159@gmail.com>
2025-08-14 07:58:50 +0530

    Number Pyramid

---
+++ numberPyramid.java
@@ -0,0 +1,16 @@
+public class numberPyramid {
+    public static void main(String[] args){
+        int rows = 5;
+
+        for(int i = 1; i <= rows; i++){
+            for(int space = 1; space <= rows - i; space++){
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
