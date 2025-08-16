83ecb54d17998b415108bf5e0aa579db7d173b80
Shree Hari P <shreehariparthiban159@gmail.com>
2025-08-16 22:05:30 +0530

    Diamond Pyramid using Star

--- diamondShape.java
+++ diamondShape.java
@@ -1,26 +1,26 @@
-public class diamondShape {
+public class diamondShape{
     public static void main(String[] args){
         int rows = 5;
-
-        //star pyramid
+        
+        //pyramid
         for(int i = 1; i < rows; i++){
-            for(int space = 1; space <= rows - i; space++){
-                System.out.print(" ");
+            for(int s = 1; s <= rows - i; s++){
+                System.out.print("  ");
             }
 
-            for(int j = 1; j <= i; j++){
+            for(int j = 1; j <= 2 * i - 1; j++){
                 System.out.print("* ");
             }
             System.out.println();
         }
 
-        //Inverted Star Pyramid
-        for(int i = rows; i >= 1; i++){
-            for(int space = 1; space <= rows - i; space++){
-                System.out.print(" ");
+        //inverted pyramid
+        for(int i = rows; i >= 1; i--){
+            for(int s = 1; s <= rows - i; s++){
+                System.out.print("  ");
             }
 
-            for(int j = 1; j <= i; j++){
+            for(int j = 1; j <= 2* i - 1; j++){
                 System.out.print("* ");
             }
             System.out.println();
